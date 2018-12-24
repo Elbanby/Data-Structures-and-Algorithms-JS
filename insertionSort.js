31b388d575587666ecc1e5ce13fde9a42cb3a5c3
omarelbanby <obanby@gmail.com>
2018-12-23 21:09:18 -0500

    Remove unnecessary comments

--- insertionSort.js
+++ insertionSort.js
@@ -1,15 +1,11 @@
 function insertionSort(array) {
   if (array.length === 1) return array;
-  //Starting left to right
   for (let i = 1 ; i < array.length ; i++) {
-    //Checking the item to the left
     for (let j = i - 1 ; j >= 0 ; j--) {
-      //Chcehk if it is smaller to the item to its left
       if (array[j] > array[i]) {
         let temp = array[i];
         array[i] = array[j];
         array[j] = temp;
-        //You decrement i is a must so you compare the new index you moved
         i--;
       } else {
         break;
@@ -18,8 +14,3 @@ function insertionSort(array) {
   }
   return array;
 }
-
-console.log(insertionSort([0]));
-console.log(insertionSort([0, -1]));
-console.log(insertionSort([0, -1, 2]));
-console.log(insertionSort([10,0, 5, 1, 4, 2]));
