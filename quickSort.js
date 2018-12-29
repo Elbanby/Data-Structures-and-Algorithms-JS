904e369a8a09ba9c4aad5daee62f35fc8b9fbc8d
elbanby <elbanby@elbanbys-MacBook-Pro.local>
2018-12-28 22:53:54 -0500

    Add quick sort

---
+++ quickSort.js
@@ -0,0 +1,31 @@
+function QuickSort(arr, li = 0, ri = arr.length - 1) {
+  if (li > ri) return arr;
+
+  let pivot = ri,
+    partitionIndex = partition(arr, pivot, li, ri);
+
+  QuickSort(arr, li, partitionIndex - 1);
+  QuickSort(arr, partitionIndex + 1, ri);
+
+  return arr;
+}
+
+function partition(arr, pivot, li, ri) {
+  let partitionIndex = li;
+
+  for (let i = li; i < ri; i++) {
+    if (arr[i] < arr[pivot]) {
+      swap(arr, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+
+  swap(arr, pivot, partitionIndex);
+  return partitionIndex;
+}
+
+function swap(arr, i1, i2) {
+  let temp = arr[i1];
+  arr[i1] = arr[i2];
+  arr[i2] = temp;
+}
